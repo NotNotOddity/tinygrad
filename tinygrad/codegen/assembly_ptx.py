39c6f340ea571effb258087902aadbd69f04d803
John Boeggeman <boeggeman97@gmail.com>
2023-07-13 16:58:21 -0500

    specific taget based on the current device

--- tinygrad/codegen/assembly_ptx.py
+++ tinygrad/codegen/assembly_ptx.py
@@ -1,6 +1,7 @@
 import struct
 from tinygrad.codegen.assembly import AssemblyCodegen
 from tinygrad.ops import BinaryOps, UnaryOps, FusedOps
+from tinygrad.runtime.ops_cuda import arch
 from tinygrad.codegen.linearizer import UOps
 from tinygrad.helpers import dtypes
 
@@ -12,7 +13,7 @@ class PTXCodegen(AssemblyCodegen):
   #supports_constant_folding: bool = True
 
   def specialize(self, asm):
-    ins = [".version 7.8", ".target sm_86", ".address_size 64",
+    ins = [".version 7.8", f".target {arch()}", ".address_size 64",
            f".visible .entry test({', '.join(f'.param .u64 buf{i}' for i in range(len(self.bufs)))}) {{"]
 
     alu = {BinaryOps.ADD: "add", BinaryOps.SUB: "sub", BinaryOps.MUL: "mul", BinaryOps.DIV: "div", BinaryOps.MAX: "max",
